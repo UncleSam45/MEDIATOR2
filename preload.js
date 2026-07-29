044c092b2b9c6374ad2d2fd47bba0923f1e07c18
Sam V. <unclesam450@pm.me>
2026-07-28 21:04:11 -0400

    Redesign desktop chrome atlas and character sheets

--- preload.js
+++ preload.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("mediatorDesktop", {
   minimize: () => ipcRenderer.invoke("window:minimize"),
+  maximize: () => ipcRenderer.invoke("window:maximize"),
   close: () => ipcRenderer.invoke("window:close"),
   openExternal: (url) => ipcRenderer.invoke("external:open", url),
   loadCredentials: () => ipcRenderer.invoke("credentials:load"),
